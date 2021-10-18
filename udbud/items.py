af7600cf0a80a5a5a0f259c46dfbb6118c332371
Rasmus Sten Andersen <rsandersen@youmail.dk>
2021-10-18 11:45:11 +0200

    added tender item

--- udbud/items.py
+++ udbud/items.py
@@ -8,5 +8,17 @@ import scrapy
 
 class UdbudItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    tender_type = scrapy.Field()
+    tender_title = scrapy.Field()
+    contractor = scrapy.Field()
+    place_of_delivery = scrapy.Field()
+    announced_date = scrapy.Field()
+    deadline_date = scrapy.Field()
+    last_edited = scrapy.Field()
+    document_type = scrapy.Field()
+    description = scrapy.Field()
+    tender_details = scrapy.Field()
+
+    def __repr__(self):
+        """Only print out tender name after exiting the Pipeline"""
+        return repr({"tender_title": self["tender_title"]})
--- udbud/spiders/udbud_spider.py
+++ udbud/spiders/udbud_spider.py
@@ -1,15 +1,12 @@
 import scrapy
+from udbud.items import UdbudItem
 
 class UdbudSpider(scrapy.Spider):
     name = "udbud"
+    allowed_domains = ["udbud.dk"]
     start_urls = [
         'https://udbud.dk/Pages/Tenders/News',
     ]
-
-    @staticmethod
-    def parse_values(value: str):
-        value = value.replace("\r\n", "").strip()
-        return value
     
     @staticmethod
     def is_clentech_tender(text: str):
@@ -17,32 +14,52 @@ class UdbudSpider(scrapy.Spider):
         pass
     
     def parse_tender(self, response):
-        meta_data = response.meta
-        for row in response.xpath('//table[@class="details-table"]//tbody//tr'):
-            key = row.xpath("td//text()")[0].get().strip()
-            val = row.xpath("td//text()")[1].get().strip()
-
-            meta_data.update({key: val})
+        data = response.meta
         
-        yield meta_data
+        table = response.xpath('//table[@class="details-table details-table-big"]')
+        if len(table) > 0:
+            rows = table[0].xpath('tr')
+        else:
+            rows = response.xpath('//table[@class="details-table"]//tbody//tr')
+        
+        for row in rows:
+            key = row.xpath("td//text()")[0].get().strip()
+            val = [text.strip() for text in row.xpath("td//text()")[1:].getall()]
+            val = " ".join(val).strip()
+
+            data.update({key: val})
+        
+        item = UdbudItem()
+        item["tender_type"] = data.get("Udbudstype")
+        item["tender_title"] = data.get("Titel")
+        item["contractor"] = data.get("Ordregiver")
+        item["place_of_delivery"] = data.get("Leveringssted")
+        item["announced_date"] = data.get("Annonceret")
+        item["deadline_date"] = data.get("Tilbudsfrist", data.get("Deadline"))
+        item["last_edited"] = data.get("Sidst ændret")
+        item["document_type"] = data.get("Dokumenttype")
+        item["description"] = data.get("Kort beskrivelse", data.get("Opgavebeskrivelse"))
+        item["tender_details"] = data.get("Udbudsdetaljer")
+
+        yield item
 
     def parse(self, response):
         for row in response.xpath('//tbody//tr'):
             
             # Get data from main table
             table_data = {
-                'Udbudstype': self.parse_values(row.xpath("td//text()")[0].get()),
-                'Titel': self.parse_values(row.xpath("td//text()")[2].get()),
-                'Ordregiver': self.parse_values(row.xpath("td//text()")[4].get()),
-                'Leveringssted': self.parse_values(row.xpath("td//text()")[5].get()),
-                'Annonceret': self.parse_values(row.xpath("td//text()")[7].get()),
-                'Tilbudsfrist': self.parse_values(row.xpath("td//text()")[8].get()),
+                'Udbudstype': row.xpath("td//text()")[0].get().strip(),
+                'Titel': row.xpath("td//text()")[2].get().strip(),
+                'Ordregiver': row.xpath("td//text()")[4].get().strip(),
+                'Leveringssted': row.xpath("td//text()")[5].get().strip(),
+                'Annonceret': row.xpath("td//text()")[7].get().strip(),
+                'Tilbudsfrist': row.xpath("td//text()")[8].get().strip(),
             }
 
             # Follow link in title
             subpage_url = row.xpath("td/a/@href").get()
             subpage_url = "https://udbud.dk" + subpage_url
-            
+
             # Follow link to get description of tender
             yield scrapy.Request(
                 url=subpage_url,
